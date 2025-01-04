9ecbb4c4826fea9690ba323df92f9a0fc34744c5
leoBitto <leonardobitto@gmail.com>
2025-01-04 18:55:42 +0100

    made template mobile first

--- urls.py
+++ urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('accounts/<int:account_id>/', AccountDetailView.as_view(), name='account_detail_view'),
     path('income/', IncomeView.as_view(), name='income_view'),
     path('expense/', ExpenseView.as_view(), name='expense_view'),
-    path('transactions/<int:transaction_id>/', TransactionDetailView.as_view(), name='transaction_detail_view'),
+    path('transaction/<int:transaction_id>/', TransactionDetailView.as_view(), name='transaction_detail_view'),
     path('categories/', CategoryView.as_view(), name='category_view'),
     path('categories/<int:category_id>/', CategoryDetailView.as_view(), name='category_detail_view'),
 ]
